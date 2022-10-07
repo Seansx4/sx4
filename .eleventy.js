30a2b7ffc856b629405add3b7e9121dbbefa975b
Sean <seansx4@gmail.com>
2022-10-07 20:55:52 +0100

    Moved to Eleventy

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig){
+
+    eleventyConfig.addPassthroughCopy('./src/css');
+    eleventyConfig.addPassthroughCopy('./src/images');
+    eleventyConfig.addPassthroughCopy('./src/js');
+
+    return{
+        dir: {
+            input:"src",
+            output: "public"
+        }
+    };
+}
