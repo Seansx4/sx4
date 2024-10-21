8a28f344b905412c447744d30693871ae2dfc215
Sean <seansx4@gmail.com>
2024-10-21 19:39:35 +1100

    Added clients, business card referral, python scraper for reviews

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/css');
     eleventyConfig.addPassthroughCopy('./src/images');
     eleventyConfig.addPassthroughCopy('./src/js');
+    eleventyConfig.addPassthroughCopy('./src/python');
 
     return{
         dir: {
