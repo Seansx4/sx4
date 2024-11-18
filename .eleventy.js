76b6858161365dcea144112bc3a9ead1de8bed81
Sean <seansx4@gmail.com>
2024-11-18 11:59:18 +1100

    301s, 404s, robots, sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,17 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addPassthroughCopy('./src/images');
     eleventyConfig.addPassthroughCopy('./src/js');
     eleventyConfig.addPassthroughCopy('./src/python');
+    eleventyConfig.addPassthroughCopy('./src/robots.txt');
+    
+    // Sitemap whitespace config
+    eleventyConfig.addTransform("xmlWhitespaceFix", function (content, outputPath) {
+    if (outputPath && outputPath.endsWith("sitemap.xml")) {
+        return content.trim();
+    }
+    return content;
+    });
+
+
 
     return{
         dir: {
